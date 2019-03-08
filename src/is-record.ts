6942c4477b721ef90f41b596c8f8b332a64cdb2a
Eric Crosson <eric.s.crosson@utexas.edu>
2019-03-07 20:50:43 -0600

    Implement type-guard

--- src/is-record.ts
+++ src/is-record.ts
@@ -3,10 +3,18 @@
  * Type-guard for Record
  */
 
+import is from '@sindresorhus/is'
+import Record from 'timeseries-record'
+
 
 /**
- * TODO: document
+ * Type-guard for `Record`.
  */
-export default function isRecord() {
-    // TODO: implement
+export default function isRecord(object: any): object is Record {
+    return !is.undefined(object.Time) && is.number(object.Time)
+        && !is.undefined(object.Open) && is.number(object.Open)
+        && !is.undefined(object.High) && is.number(object.High)
+        && !is.undefined(object.Low) && is.number(object.Low)
+        && !is.undefined(object.Close) && is.number(object.Close)
+        && !is.undefined(object.Volume) && is.number(object.Volume)
 }
--- test/test-is-record.ts
+++ test/test-is-record.ts
@@ -1,4 +1,5 @@
 import test from 'ava'
+import Record from 'timeseries-record'
 
 /**
  * Library under test
@@ -6,8 +7,193 @@ import test from 'ava'
 
 import isRecord from '../src/is-record'
 
-test('test ava configuration', t => {
-    t.pass()
+test('should recognize Records', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.true(isRecord(record))
 })
 
-// TODO: write tests
+test('should recognize Records with extra members', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0,
+        extra: 'member'
+    }
+    t.true(isRecord(record))
+})
+
+test('should not recognize Records without Time', t => {
+    const record = {
+        // Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records without Open', t => {
+    const record = {
+        Time: 0,
+        // Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records without High', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        // High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records without Low', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        // Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records without Close', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        // Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records without Volume', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        // Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records with Time of improper type', t => {
+    const record = {
+        Time: '0',
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records with Open of improper type', t => {
+    const record = {
+        Time: 0,
+        Open: '0',
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records with High of improper type', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: '0',
+        Low: 0,
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records with Low of improper type', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: '0',
+        Close: 0,
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records with Close of improper type', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: '0',
+        Volume: 0
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize Records with Volume of improper type', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: '0'
+    }
+    t.false(isRecord(record))
+})
+
+test('should not recognize an empty object', t => {
+    t.false(isRecord({}))
+})
+
+test('should function as a type-guard', t => {
+    const record = {
+        Time: 0,
+        Open: 0,
+        High: 0,
+        Low: 0,
+        Close: 0,
+        Volume: 0,
+        oldMacDonaldHadA: 'horse'
+    }
+    if (isRecord(record)) {
+        let record2: Record = record
+        t.pass()
+    } else {
+        t.fail()
+    }
+})
